dcbdd0def2dc4a1a30473d1323157ca5d4a0a6a7
Gerry Munn <randword@gnet.scot>
2019-11-25 22:06:06 +0000

    Apply prefixes before suffixes

--- randword.py
+++ randword.py
@@ -96,65 +96,12 @@ def apply_affixes(base_word, affix_rules):
     complete_words.extend([
             apply_suffix(base_word, affix_rule[3:])
             for affix_rule in affix_rules
-            if affix_rule[0] == "SFX" and not affix_rule[1]
+            if affix_rule[0] == "PFX" and not affix_rule[1]
     ])
     complete_words.extend([
             apply_prefix(base_word, affix_rule[3:])
             for affix_rule in affix_rules
-            if affix_rule[0] == "PFX" and not affix_rule[1]
-    ])
-    partial_words.extend([
-            apply_suffix(base_word, affix_rule[3:])
-            for affix_rule in affix_rules
-            if affix_rule[0] == "SFX" and affix_rule[1]
-    ])
-    for partial_word in partial_words[:]:
-        partial_words.extend([
-                apply_prefix(partial_word, affix_rule[3:])
-                for affix_rule in affix_rules
-                if affix_rule[0] == "PFX" and affix_rule[1]
-        ])
-    complete_words.extend(partial_words)
-    
-    return complete_words
-
-
-def apply_affixes_2(base_word, affix_rules):
-    """
-    Build all possible words by applying affix rules to a base word
-    
-    Parameters:
-    base_word (string):  Base word without any affix specifiers
-    affix_rules (list):  Affix rules to be applied
-    
-    Returns:
-    List object containing all words formed by applying
-    affixes to the base word.
-    """
-
-    # Begin with base word in partial
-    # Apply standalone suffixes to base word and add to complete
-    # Apply standalone prefixes to base word and add to complete
-    # Apply mixable suffixes to base word and add to partial
-    # Apply mixable prefixes to partial
-    # Add partial to complete
-    
-    # TODO: Test if it is quicker to add prefixes before suffixes
-
-    complete_words = []
-    partial_words = []
-    
-    partial_words.append(base_word)
-    
-    complete_words.extend([
-            apply_suffix(base_word, affix_rule[3:])
-            for affix_rule in affix_rules
             if affix_rule[0] == "SFX" and not affix_rule[1]
-    ])
-    complete_words.extend([
-            apply_prefix(base_word, affix_rule[3:])
-            for affix_rule in affix_rules
-            if affix_rule[0] == "PFX" and not affix_rule[1]
     ])
     partial_words.extend([
             apply_prefix(base_word, affix_rule[3:])
@@ -267,95 +214,6 @@ def get_affix_rules(affix_file):
             file_open = True
             rule_count = 0
             for line in f:
-                if not re.match("^[PS]FX\s", line):
-                    continue
-                else:
-                    rule_data = line.split()
-                    if rule_count == 0:
-                        rule_type = rule_data[0]
-                        rule_id = rule_data[1]
-                        can_combine = (
-                                True
-                                if rule_data[2] == "Y"
-                                else False
-                        )
-                        rule_count = int(rule_data[3])
-                        rule_dict[rule_id] = [
-                                rule_type, 
-                                can_combine, 
-                                rule_count
-                        ]
-                    else:
-                        rule_id = rule_data[1]
-                        rule_data[2] = (
-                                ''
-                                if rule_data[2] == "0"
-                                else rule_data[2]
-                        )
-                        rule_data[4] = (
-                                "^" + rule_data[4][:]
-                                if rule_type == "PFX"
-                                else rule_data[4][:] + "$"
-                        )
-                        rule_dict[rule_id].append(tuple(rule_data[2:]))
-                        rule_count -= 1
-                
-    except OSError:
-        error_type = "reading" if file_open else "opening"
-        print(
-                "Error "
-                + error_type
-                + " affix file "
-                + affix_file, 
-                file=sys.stderr
-        )
-        
-    return rule_dict
-
-
-def get_affix_rules_mmap(affix_file):
-    """
-    Read affix rules from a specified file and return these
-    in a structured format
-    
-    Parameters:
-    affix_file (string):  Path to affix file
-    
-    Returns:
-    Dict object with the following structure:
-        Key:    Identifying letter for this affix
-        Value:  List object containing the following objects:
-                    [0]: String object containing the value "PFX" or
-                         "SFX" to indicate the affix type
-                    [1]: Boolean object indicating whether this affix
-                         can be combined with other affixes 
-                         (only 1 prefix and 1 suffix can be added to
-                          a base word)
-                    [2]: Integer object indicating how many match rules
-                         exist for this affix
-                    [3]: 1 or more tuple objects containing the match
-                         rules, as follows:
-                         [0]: Any letter(s) to be removed from the word
-                              (Remove from start of word for prefixes
-                               and from end for suffixes, although
-                               prefixes do not generally require any
-                               removal)
-                         [1]: Letters to be added to the word for this
-                              rule
-                              (Add to start for prefix, add to end for
-                               suffix)
-                         [2]: Regex expression to be matched for this
-                              rule
-    """
-    rule_dict = {}
-    file_open = False
-    try:
-        with open(affix_file, "rb") as f:
-            mm = mmap.mmap(f.fileno(), 0, prot=mmap.PROT_READ)
-            file_open = True
-            rule_count = 0
-            for b_line in iter(mm.readline, b""):
-                line = str(b_line, "utf-8")
                 if not re.match("^[PS]FX\s", line):
                     continue
                 else:
